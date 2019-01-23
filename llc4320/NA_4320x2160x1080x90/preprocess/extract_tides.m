7ca008004fd4d986b2c881d95b2e798f0b26cd01
Ivana Escobar <ivana@utexas.edu>
2019-01-23 12:45:53 -0600

    added tidal forcing

---
+++ llc4320/NA_4320x2160x1080x90/preprocess/extract_tides.m
@@ -0,0 +1,153 @@
+% This script extracts the TPXO tidal predictions at the OBC locations using
+% TMD, converts them into the format used by the MITgcm, and saves the
+% information to input files.
+
+%addpath(genpath('/net/barents/raid16/vocana/llc4320/NA2160x1080/run_template/joernc/tides/tmd_mar_203/TMD2.03'));
+%addpath(genpath('/data4/joernc/MITgcm/utils/matlab'))
+addpath('/home/atnguyen/nansen/tides/JCcode_tides_obcs/tmd_mar_203/TMD2.03');
+addpath('/home/atnguyen/nansen/tides/JCcode_tides_obcs/tmd_mar_203/TMD2.03/FUNCTIONS/');
+addpath('/home/atnguyen/nansen/tides/JCcode_tides_obcs/tmd_mar_203/TMD2.03/DATA/');
+addpath('/home/atnguyen/nansen/tides/JCcode_tides_obcs/tmd_mar_203/TMD2.03/LAT_LON/');
+addpath('/home/atnguyen/nansen/tides/JCcode_tides_obcs')
+dirIn='/scratch/ivana/llc/llc4320/NA_4320x2160x1080x90/run_template/input_obcs/';
+dirOut=dirIn;
+
+% model start time
+time = datenum(2002,01,01);
+
+% pad lengths for other faces
+nz=90;
+nx = 2160; ncut2 = 1080;
+padN = ncut2;
+padS = ncut2;
+padW = ncut2;
+padE1_1 = 527; % ncut2-553
+padE1_2 = 540;
+
+% load OBC locations
+load([dirIn 'latlon_for_obcs_tides.mat'],'Face1N','Face1S','Face5E','Face5W','Face1E');
+
+%******** WARNING *********
+% Need to be careful here as uE,vN only works where llc_grid
+% is pure E/N, for example at some latitude it's transitioning
+% into projection, in which case will need to rotate to model
+% u/v using Angle[CS,SN].
+% Luckily, for this set up, 
+% face4: yc4(495,181:1080) are identical, 50.708789825439453 degN
+% face1: yc1(1:360,61) are identical,      4.671571254730225 degN
+%**************************
+
+% NORTH
+% face 1: normal component: vN at N, save as v at N
+% face 1: tangential component: uE at N, save as u at N
+n1N = nx+ncut2; %2160+1080 = 3240 (whole length)
+for iloop=1:2;
+  clear am ph type
+  if(iloop==1);type='v';str='';else;type='u';str='t';end;
+  [am,ph,h,cl] = tmd_extract_HC('DATA/Model_tpxo7.2',Face1N.yg,Face1N.xc,type); %am: m, ph: deg [0-360]
+  [am,ph] = conv_corr(time,am,ph,cl);   %am: m, ph: phase in sec 
+  am(isnan(am)) = 0;		% 13 x 4320?
+  ph(isnan(ph)) = 0;		% 13 x 4320?
+  am = [am,zeros(length(cl),padS)]; % pad for face 5, 13 x 3240
+  ph = [ph,zeros(length(cl),padS)]; % pad for face 5, 13 x 3240
+  writebin([dirOut 'OBN' str 'am_' num2str(n1N) 'x' num2str(size(cl,1)) '.bin'],am',1,'real*4'); %  amplitude
+  writebin([dirOut 'OBN' str 'ph_' num2str(n1N) 'x' num2str(size(cl,1)) '.bin'],ph',1,'real*4'); %  phase
+  clear am ph
+end;
+
+% SOUTH
+% face 1: normal component: vN at S, save as v at S
+% face 1: tangential component: uE at S, save as u at S
+n1S = nx+ncut2; %2160+1080 = 3240 (whole length)
+for iloop=1:2;
+  clear am ph type
+  if(iloop==1);type='v';str='';else;type='u';str='t';end;
+  [am,ph,h,cl] = tmd_extract_HC('DATA/Model_tpxo7.2',Face1S.yg,Face1S.xc,type); %am: m, ph: deg [0-360]
+  [am,ph] = conv_corr(time,am,ph,cl);   %am: m, ph: phase in sec 
+  am(isnan(am)) = 0;		% 13 x 4320?
+  ph(isnan(ph)) = 0;		% 13 x 4320?
+  am = [am,zeros(length(cl),padS)]; % pad for face 1, 13 x 3240
+  ph = [ph,zeros(length(cl),padS)]; % pad for face 1, 13 x 3240
+  writebin([dirOut 'OBS' str 'am_' num2str(n1S) 'x' num2str(size(cl,1)) '.bin'],am',1,'real*4'); %  amplitude
+  writebin([dirOut 'OBS' str 'ph_' num2str(n1S) 'x' num2str(size(cl,1)) '.bin'],ph',1,'real*4'); %  phase
+  clear am ph
+end;
+
+% WEST: check
+% face 5: normal    : vN at N, save as -u at W
+% face 5: tangential: uE at N, save as +v at W
+n5W = nx+ncut2; %2160+1080 = 3240 (whole length)
+bc = 'n5W';
+for iloop=1:2;
+  clear am ph type
+  if(iloop==1);type='v';str='';ss=-1.;else;type='u';str='t';ss=1.;end;
+  [am,ph,h,cl] = tmd_extract_HC('DATA/Model_tpxo7.2',Face5W.yg,Face5W.xc,type); %am: m, ph: deg [0-360]
+  [am,ph] = conv_corr(time,am,ph,cl);   %am: m, ph: phase in sec 
+  am(isnan(am)) = 0;		% 13 x 4320?
+  ph(isnan(ph)) = 0;		% 13 x 4320?
+  am = [am,zeros(length(cl),padS)]; am=ss.*am   % pad for face 5, 13 x 3240
+  ph = [ph,zeros(length(cl),padS)];             % pad for face 5, 13 x 3240
+  writebin([dirOut 'OBW' str 'am_' num2str(n1S) 'x' num2str(size(cl,1)) '.bin'],am',1,'real*4'); %  amplitude
+  writebin([dirOut 'OBW' str 'ph_' num2str(n1S) 'x' num2str(size(cl,1)) '.bin'],ph',1,'real*4'); %  phase
+  clear am ph
+end;
+
+% EAST
+%Gibraltar Strait:
+% face 1: normal: uE at E, save as u at E
+% face 1: tangential: vN at E, save as v at E
+clear am ph type uam uph vam vph
+str='1E';
+for iloop=1:2;
+  if (iloop==1);type='u';else;type='v';end;
+  [am,ph,h,cl] = tmd_extract_HC('DATA/Model_tpxo7.2',Face1E.yc,Face1E.xg,type);
+  [am,ph] = conv_corr(time,am,ph,cl);
+  eval([type 'am' str '=am;']);eval([type 'ph' str '=ph;']);
+  clear am ph type
+end;
+
+% face 5: normal    : vN at S, save as -u at E
+% face 5: tangential: uE at S, save as +v at E
+str='5E';
+for iloop=1:2;
+  if(iloop==1);type='v';ss='-1.0';else;type='u';ss='1.0';end;
+  [am,ph,h,cl] = tmd_extract_HC('DATA/Model_tpxo7.2',Face5E.yg,Face5E.xc,type);
+  [am,ph] = conv_corr(time,am,ph,cl);
+  eval([type 'am' str '=' ss '.*am;']);eval([type 'ph' str '=ph;']);
+  clear am ph
+end;
+
+% Putting all East together:
+nE = nx+ncut2;% 2160 + 1080 = 3240
+str='';
+uam = cat(2,uam1E, vam5E); uam(isnan(uam)) = 0;
+uph = cat(2,uph1E, vph5E); uph(isnan(uph)) = 0;
+
+writebin([dirOut 'OBE' str 'am_' num2str(4500) 'x' num2str(size(cl,1)) '.bin'],uam',1,'real*4'); % amplitude
+writebin([dirOut 'OBE' str 'ph_' num2str(4500) 'x' num2str(size(cl,1)) '.bin'],uph',1,'real*4'); % phase
+
+str='t';
+vam = cat(2,vam1E, uam5E); vam(isnan(vam)) = 0;
+vph = cat(2,vph1E, uph5E); vph(isnan(vph)) = 0;
+
+writebin([dirOut 'OBE' str 'am_' num2str(nE) 'x' num2str(size(cl,1)) '.bin'],vam',1,'real*4'); % amplitude
+writebin([dirOut 'OBE' str 'ph_' num2str(nE) 'x' num2str(size(cl,1)) '.bin'],vph',1,'real*4'); % phase
+
+% save constituent periods to periods.txt
+periods = zeros(length(cl),1);
+for i = 1:length(cl)
+  [ispec,am,ph,omega,alpha,constitNum] = constit(cl(i,:));
+  periods(i) = 2*pi/omega;
+end
+fileID = fopen([dirOut 'periods.txt'],'w');
+fprintf(fileID,'name:        ');
+for i = 1:length(cl)
+  fprintf(fileID,'%4s         ',cl(i,:));
+end
+fprintf(fileID,'\n');
+fprintf(fileID,'period (hr): ');
+fprintf(fileID,'%12.6f ',periods'/3600);
+fprintf(fileID,'\n');
+fprintf(fileID,'period (s):  ');
+fprintf(fileID,'%12.6f ',periods');
+fclose(fileID);
--- llc4320/NA_4320x2160x1080x90/preprocess/get_latlon_tidesobcs.m
+++ llc4320/NA_4320x2160x1080x90/preprocess/get_latlon_tidesobcs.m
@@ -1,55 +1,57 @@
-clear all;
-warning off
+clear all; clc;
+warning off;
 
-%for aste_1080x1440x540x90:
-% OB_Jsouth =   1080*256, 3060*0,
-% OB_Ieast  =   680*0, 4*387, 756*0, 1080*0, 1080*494, 1080*1185,
-%these are the first "wet point", but need to use the velocity points for tides, so it would have been:
-% face1.S              face4.E            face5.E               face1.E
-%[obcs2{1}.jvel(2,1) obcs2{2}.ivel(2,1) obcs2{3}.ivel(2,1) obcs2{4}.ivel(2,1)]	[257 494 1185 387]
+% Take velocity points as first available points. Ex: Face 1 will shift jC1
+% for South OBC and iC1 for West OBC.
+% From namelists/data:
+%   OB_Jnorth = 2160*1057, 1080*0, 
+%   OB_Jsouth = 2160*16  , 1080*0, 
+%   OB_Ieast  = 539*0, 14*1531, 527*0, 2160*1065,
+%   OB_Iwest  = 1080*0   , 2160*24, 
+Face1N.jvel=[10433 1057]';  Face1N.jC1=[10433 1057]';
+Face1S.jvel=[9393    17]';  Face1S.jC1=[9392    16]';
+Face5E.ivel=[3569  1065]';  Face5E.iC1=[3569  1065]';
+Face5W.ivel=[2529    25]';  Face5W.iC1=[2528    24]';
+Face1E.ivel=[1531  1531]';  Face1E.iC1=[1531  1531]';
 
-%so can use the same logic for aste_1080x1260x540x90 , where obcs were not extracted from scratch
-%but from aste_1080x1440x540x90 (actually global pts are all at the same place, dried points are 
-%more inside in aste_1080x1260x540x90 than aste_1080x1440x540x90
-% OB_Jsouth =   1080*76, 3060*0,
-% OB_Ieast  =   500*0, 4*387, 756*0, 1080*0, 1080*494, 1080*1185,
-Face1S.jvel=[2057   77]';  Face1S.jC1=[2056   76]';
-Face4E.ivel=[ 494  494]';  Face4E.iC1=[ 494  494]';
-Face5E.ivel=[1185 1185]';  Face5E.iC1=[1185 1185]';
-Face1E.ivel=[ 387  387]';  Face1E.iC1=[ 387  387]';
-
-dirRoot='/home/atnguyen/llc1080/aste_1080x1260x540x90/run_template/';
+dirRoot='/scratch/ivana/llc/llc4320/NA_4320x2160x1080x90/run_template';
 dirOut=[dirRoot 'input_obcs/'];
-nx=1080;
-nfx=[nx 0 nx 540 1260];nfy=[1260 0 nx nx nx];
+nx=2160;
+ncut2=1080;
+ny=2*ncut2;
+nfx=[nx 0 0 0 ncut2];nfy=[ncut2 0 0 0 nx];
+
+b=readbin([dirRoot 'input_binaries/SandSv18p1_NA4320x2160x1080_obcs13Jul2018.bin'],[nx ny]);
+[b,bf]=get_aste_tracer(abs(b),nfx,nfy);
+hf=readbin([dirRoot '../GRID/hFacC.data'],[nx ny]);hf(find(hf>0))=1;
+[hf,hff]=get_aste_tracer(hf,nfx,nfy);
+
+flist=dir([dirRoot 'input_binaries/tile*.mitgrid']);
 
 list_fields2={'XC','YC','DXF','DYF','RAC','XG','YG','DXV','DYU','RAZ',...
     'DXC','DYC','RAW','RAS','DXG','DYG'};
 
-b=readbin([dirRoot 'bathy_aste1080x1260x540_obcs13Jan2018_v2.bin'],[nx 2*nfy(1)+nx+nfx(4)]);
-[b,bf]=get_aste_tracer(abs(b),nfx,nfy);
-hf=readbin([dirRoot '../GRID/hFacC.data'],[nx 2*nfy(1)+nx+nfx(4)]);hf(find(hf>0))=1;
-[hf,hff]=get_aste_tracer(hf,nfx,nfy);
-
-flist=dir([dirRoot 'tile*.mitgrid']);
-
 i=1;
 yg=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,7,'real*8');
 xc=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,1,'real*8');
-Face1S.yg=yg(1:nfx(i),Face1S.jvel(2,1));
-Face1S.xc=xc(1:nfx(i),Face1S.jC1(2,1));	%outside point
+Face1N.yg=yg(1:nfx(i),Face1N.jvel(2,1));
+Face1N.xc=xc(1:nfx(i),Face1N.jC1(2,1)); %outside point
 
-i=4;
 yg=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,7,'real*8');
 xc=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,1,'real*8');
-Face4E.yg=yg(Face4E.ivel(2,1),1:nfy(i));
-Face4E.xc=xc(Face4E.iC1(2,1) ,1:nfy(i));
+Face1S.yg=yg(1:nfx(i),Face1S.jvel(2,1));
+Face1S.xc=xc(1:nfx(i),Face1S.jC1(2,1));	
 
 i=5;
 yg=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,7,'real*8');
 xc=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,1,'real*8');
 Face5E.yg=yg(Face5E.ivel(2,1),1:nfy(i));
 Face5E.xc=xc(Face5E.iC1(2,1) ,1:nfy(i));
+
+yg=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,7,'real*8');
+xc=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,1,'real*8');
+Face5W.yg=yg(Face5W.ivel(2,1),1:nfy(i));
+Face5W.xc=xc(Face5W.iC1(2,1) ,1:nfy(i));
 
 i=1;
 xg=read_slice([dirRoot flist(i).name],nfx(i)+1,nfy(i)+1,6,'real*8');
@@ -58,26 +60,26 @@ Face1E.xg=xg(Face1E.ivel(2,1),1:nfy(i));
 Face1E.yc=yc(Face1E.iC1(2,1) ,1:nfy(i));
 
 fsave=[dirOut 'latlon_for_obcs_tides.mat'];
-save(fsave,'Face1S','Face4E','Face5E','Face1E');
+save(fsave,'Face1N','Face1S','Face5E','Face5W', 'Face1E');
 
-%note the indices might look off between face5 and face1, but the yg identically lined up.
+% NOTE: indices might look off between face5 and face1, but yg identically lined up.
 load coast;
 figure(1);clf;
 subplot(221);
-plot(long,lat,'k-',Face1S.xc,Face1S.yg,'r.',Face5E.xc,Face5E.yg,'b.',...
-                   Face4E.xc,Face4E.yg,'g.',Face1E.xg,Face1E.yc,'m.');grid;
-
+plot(long,lat,'k-', Face1N.xc,Face1N.yg,'r.',Face1S.xc,Face1S.yg,'g.',...
+                    Face5E.xc,Face5E.yg,'c.',Face5W.xc,Face5W.yg,'b.',...
+                    Face1E.xc,Face1E.yg,'m.');grid;
 subplot(222);mypcolor(bf{1}');caxis([0 3e3]);mythincolorbar;hold on;
- plot(1:nfx(1),Face1S.jvel(2,1).*ones(1,nfx(1)),'k.-');
- plot(Face1E.ivel(2,1).*ones(1,nfy(1)),1:nfy(1),'k.-');
- grid;hold off;title('Face1S,Face1E');
- %shadeland(1:nfx(1),1:nfy(1),1-hff{1}',.7.*[1 1 1]);
-subplot(223);mypcolor(bf{4}');caxis([0 3e3]);mythincolorbar;hold on;
- plot(Face4E.ivel(2,1).*ones(1,nfy(4)),1:nfy(4),'k.-');grid;hold off;title('Face4E');
- %shadeland(1:nfx(4),1:nfy(4),1-hff{4}',.7.*[1 1 1]);
+    plot(1:nfx(1),Face1N.jvel(2,1).*ones(1,nfx(1)),'k.-');
+    plot(1:nfx(1),Face1S.jvel(2,1).*ones(1,nfx(1)),'k.-');
+    plot(Face1E.ivel(2,1).*ones(1,nfy(1)),1:nfy(1),'k.-');
+    grid;hold off;title('Face1N,Face1S,Face1E');
+    %shadeland(1:nfx(1),1:nfy(1),1-hff{1}',.7.*[1 1 1]);
 subplot(224);mypcolor(bf{5}');caxis([0 3e3]);mythincolorbar;hold on;
- plot(Face5E.ivel(2,1).*ones(1,nfy(5)),1:nfy(5),'k.-');grid;hold off;title('Face5E');
- %shadeland(1:nfx(5),1:nfy(5),1-hff{5}',.7.*[1 1 1]);
+    plot(Face5E.ivel(2,1).*ones(1,nfy(5)),1:nfy(5),'k.-');
+    plot(Face5W.ivel(2,1).*ones(1,nfy(5)),1:nfy(5),'k.-');
+    grid;hold off;title('Face5E');
+    %shadeland(1:nfx(5),1:nfy(5),1-hff{5}',.7.*[1 1 1]);
 
 set(gcf,'paperunits','inches','paperposition',[0 0 12 10]);
 fpr=[dirOut 'latlon_for_obcs_tides.png'];print(fpr,'-dpng');
