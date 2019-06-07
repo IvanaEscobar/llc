f31761aa8dfba2e98201b708463f0d325b73704f
Ivana Escobar <ivana@utexas.edu>
2019-06-07 18:00:47 -0500

    2yr spinup in sverdrup

--- llc4320/NA_4320x2160x1080x90/root/script_c67c_NA4320x2160x1080x90.bash
+++ llc4320/NA_4320x2160x1080x90/root/script_c67c_NA4320x2160x1080x90.bash
@@ -1,67 +1,45 @@
 #!/bin/bash -x
-#SBATCH -J NA43t5
+#SBATCH -J NA43202yr
 #SBATCH -o bash.out/NA4320.%j.out
 #SBATCH -e bash.out/NA4320.%j.err
 ## A time limit of 0 means no time limit is to be set
 #SBATCH -t 0
-##SBATCH -N 25
-##SBATCH -n 698
 #SBATCH -N 11
 #SBATCH -n 257
-###SBATCH -A Transfer-of-ECCO-glo
 #SBATCH --mail-user=ivana@utexas.edu
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
+#SBATCH --mail-type=all
 
 #--- 0.load modules ------
 
 #---- 1.set variables ------
-#nprocs=698
-#snx=72
-#sny=72
 nprocs=257
 snx=120
 sny=120
 
-# ---------- (1) 1 day simu ----------
-#pickupts1="0000000000"
-# ---------- (2) 3 day simu ----------
-#pickupts0="0000000000"
-#pickupts1="0000001920"
-# ---------- (3) 1 week simu ----------
-#pickupts0="0000001920"
-#pickupts1="0000005760"
-# ---------- (4) 4 weeks simu ----------
-#pickupts0="0000005760"
-#pickupts1="0000010560"
-# ---------- (5) 12 weeks simu ----------
 pickupts0="0000010560"
-pickupts1="0000028080"
-# ---------- (6) 24 weeks simu ----------
-#pickupts0="0000028080"
-#pickupts1="0000059040"
+pickupts1="0000037440"
 
-forwadj= 
+forwadj=""
 vers="_c67c"
 ext3="_tidal_bc"
 jobfile=script${vers}_NA4320x2160x1080x90.bash
 
 #--- 2.set dir ------------
-srcDir=$PWD #$HOME/llc/llc4320/NA_4320x2160x1080x90/root/ ln -s in mysetups/
-buildDir=$srcDir/build${vers}
-codeDir=$srcDir/code${vers}
-inpDir=$srcDir/namelists${vers}
-scratchDir=/scratch/ivana/llc/llc4320/NA_4320x2160x1080x90
-dataDir=$scratchDir/run_template
-exeDir=$scratchDir/run${vers}${ext3}_pk${pickupts1}
+srcdir=$PWD #$HOME/llc/llc4320/NA_4320x2160x1080x90/root/ ln -s in mysetups/
+builddir=$srcdir/build${vers}
+codedir=$srcdir/code${vers}
+inputdir=$srcdir/namelists${vers}
+scratchdir=/scratch/ivana/llc/llc4320/NA_4320x2160x1080x90
+datadir=$scratchdir/run_template
+exedir=$scratchdir/run${vers}${ext3}_pk${pickupts1}
 
-if [ ! -d $exeDir ]; then
-  mkdir -p $exeDir;
-  mkdir -p $exeDir/diags;
+if [ ! -d $exedir ]; then
+  mkdir -p $exedir;
+  mkdir -p $exedir/diags;
 fi
-cd $exeDir;
+cd $exedir;
 
-cp -rf ${codeDir}/ .
+cp -rf ${codedir}/ .
 
 #--- 3. link forcing -------------
 if [ ! -d ./jra55 ]; then
@@ -70,32 +48,32 @@ if [ ! -d ./jra55 ]; then
 fi
 
 #--- 4. linking binary ---------
-ln -s ${dataDir}/input_binaries/* .
-ln -s ${dataDir}/input_pickup/* .
-ln -s ${dataDir}/input_obcs/*.bin . 
+ln -s ${datadir}/input_binaries/* .
+ln -s ${datadir}/input_pickup/* .
+ln -s ${datadir}/input_obcs/*.bin . 
 
 #==============================================================================
 #--- 5. linking xx_ fields ------
-###ln -s ${dataDir}/ADXXfiles${xxext}/xx*${xxiter}* ./
+###ln -s ${datadir}/ADXXfiles${xxext}/xx*${xxiter}* ./
 #==============================================================================
 
 #--- 6. NAMELISTS ---------
-cp -f ${srcDir}/${jobfile} .
-cp -f ${inpDir}/* .
+cp -f ${srcdir}/${jobfile} .
+cp -f ${inputdir}/* .
 \rm -f data.exch2
 cp -f data.exch2_${snx}x${sny}x${nprocs} data.exch2
 
 #--- 7. executable --------
 \rm -f mitgcmuv*
-cp -f ${buildDir}/mitgcmuv${forwadj}_${snx}x${sny}x${nprocs} ./mitgcmuv${forwadj}
+cp -f ${builddir}/mitgcmuv${forwadj}_${snx}x${sny}x${nprocs} ./mitgcmuv${forwadj}
 
 #--- 8. pickups -----------
 #NOTE: for pickup: copy instead of link to prevent accidental over-write
 #\rm -f pickup*
 if [[ ${pickupts0} ]]; then
-  pkupDir=$scratchDir/run${vers}${ext3}_pk${pickupts0}
-  cp -f ${pkupDir}/pickup.${pickupts1}.data ./pickup.${pickupts1}.data
-  cp -f ${pkupDir}/pickup.${pickupts1}.meta ./pickup.${pickupts1}.meta
+  pickupdir=$scratchdir/run${vers}${ext3}_pk${pickupts0}
+  cp -f ${pickupdir}/pickup.${pickupts1}.data ./pickup.${pickupts1}.data
+  cp -f ${pickupdir}/pickup.${pickupts1}.meta ./pickup.${pickupts1}.meta
 fi
 
 #--- 9. make a list of all linked files ------
@@ -112,7 +90,6 @@ ls -l tile* >> command_ln_binary
 #EOF
 
 #--- 11. run ----------------------------------
-set -x
 date > run.MITGCM.timing
-mpiexec --mca btl ^tcp,openib --mca mtl psm2 ${exeDir}/mitgcmuv${forwadj}  
+mpiexec --mca btl ^tcp,openib --mca mtl psm2 ${exedir}/mitgcmuv${forwadj}  
 date >> run.MITGCM.timing
